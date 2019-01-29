0593c2300ea3228185af4eb11c59786fecbcafaa
Gyokan <gyokan.nezhdetov@gmail.com>
2019-01-29 11:27:06 +0200

    Fixed validations and added password fields in the register form

--- src/app/user/register.component.ts
+++ src/app/user/register.component.ts
@@ -2,34 +2,47 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  templateUrl: './register.component.html',
+  templateUrl: 'register.component.html',
 })
 export class RegisterComponent implements OnInit {
+  readonly USERNAME_REGEX = new RegExp('^[A-Za-z0-9_\\-\\.]+$');
+  
   registerForm: FormGroup;
+  isSubmitted = false;
 
   constructor(private fb: FormBuilder) {
   }
 
   ngOnInit() {
     this.registerForm = this.fb.group({
-      username: ['', [Validators.required, Validators.minLength(3)]],
-      firstName: ['', [Validators.required, Validators.maxLength(50)]],
-      lastName: ['', [Validators.required, Validators.maxLength(50)]],
+      username: ['', [Validators.required, Validators.minLength(3), Validators.pattern(this.USERNAME_REGEX)]],
+      firstName: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(50)]],
+      lastName: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(50)]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', Validators.required]
+      // confirmPassword: ['', Validators.required]
     });
+  }
+
+  get formControls() {
+    return this.registerForm.controls;
   }
 
   onSubmit() {
     console.log('Form submitted!');
     console.log('Todo: send request to server');
     console.log(this.registerForm);
-    console.log(JSON.stringify(this.registerForm.value));
+    console.log(this.registerForm.value);
   }
 
+  // Only for testing
   populateTestData(): void {
     this.registerForm.patchValue({
       username: 'ivancho_1998',
       firstName: 'Ivan',
       lastName: 'Petrov',
+      password: '123456',
+      confirmPassword: '123456',
       // email: 'ivan.petrov@gmail.com'
     });
   }
