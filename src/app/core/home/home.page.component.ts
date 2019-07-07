8f6b4b92d5980e4350b4fac942747e9a89395ea7
Gyokan <syuleymanovg@vmware.com>
2019-07-07 09:25:28 +0300

    Added initial app info data

--- src/app/core/home/home.page.component.ts
+++ src/app/core/home/home.page.component.ts
@@ -1,8 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+import { ServerResponse } from '../../shared/models/server-response.model';
+
+interface AppInfo {
+   quizzes: any[];
+   totalQuizzes: number;
+   totalSolvedQuizzes: number;
+   totalQuestions: number;
+   totalUsers: number;
+}
 
 @Component({
-  templateUrl: './home.page.component.html'
+   templateUrl: './home.page.component.html'
 })
-export class HomePageComponent {
-  public pageTitle = 'Welcome to Quiz Leaner!';
+export class HomePageComponent implements OnInit {
+   private readonly baseUrl = environment.apiHost + 'quiz';
+   public pageTitle = 'Welcome to Quiz Leaner!';
+
+   totalQuizzesCount: number;
+   totalQuestions: number;
+   totalUsers: number;
+
+   constructor(private http: HttpClient) {
+   }
+
+   ngOnInit(): void {
+      this.fetchAppData();
+   }
+
+   private fetchAppData(): void {
+      this.http.get(this.baseUrl + '/getMostRecent').subscribe((response: ServerResponse) => {
+         if (response.data) {
+            const data: AppInfo = response.data;
+            this.totalQuizzesCount = data.totalQuizzes;
+            this.totalQuestions = data.totalQuestions;
+            this.totalUsers = data.totalUsers;
+         }
+      });
+   }
 }
