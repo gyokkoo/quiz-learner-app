1d4f1aa3797c77e8e03c0ac5625397cb05868065
Nezhdetov <gyokan.nezhdetov@gmail.com>
2019-07-07 10:06:18 +0300

    Introduce material and Package-lock.json fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,18 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ToastrModule } from 'ngx-toastr';
 
+import { MatButtonModule, MatCheckboxModule } from '@angular/material';
+
 import { AppComponent } from './app.component';
-import { NavbarComponent} from './navbar/navbar.component';
+import { NavbarComponent } from './navbar/navbar.component';
 import { Error404Component } from './errors/404.component';
 import { ErrorsHandler } from './errors/errors-handler';
 import { RouterModule } from '@angular/router';
 
 import {
-  HomePageComponent,
-  AboutPageComponent
-} from './core/index';
+   HomePageComponent,
+   AboutPageComponent
+} from './core';
 
 import { appRoutes } from './routes';
 import { QuizzesModule } from './quizzes/quizzes.module';
@@ -23,36 +25,39 @@ import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent,
-    Error404Component,
-    HomePageComponent,
-    AboutPageComponent
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    NgbModule,
-    QuizzesModule,
-    ToastrModule.forRoot({
-      positionClass: 'toast-top-right',
-      preventDuplicates: false,
-    }),
-    HttpClientModule,
-    RouterModule.forRoot(appRoutes),
-    ServiceWorkerModule.register('ngsw-worker.js', {
-      enabled: environment.production
-    }),
-  ],
-  providers: [
-    {
-      provide: ErrorHandler,
-      useClass: ErrorsHandler
-    }
-  ],
-  entryComponents: [
-  ],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      NavbarComponent,
+      Error404Component,
+      HomePageComponent,
+      AboutPageComponent
+   ],
+   imports: [
+      BrowserModule,
+      BrowserAnimationsModule,
+      // NoopAnimationsModule, // <-- Disable animations
+      MatButtonModule,
+      MatCheckboxModule,
+      NgbModule,
+      QuizzesModule,
+      ToastrModule.forRoot({
+         positionClass: 'toast-top-right',
+         preventDuplicates: false,
+      }),
+      HttpClientModule,
+      RouterModule.forRoot(appRoutes),
+      ServiceWorkerModule.register('ngsw-worker.js', {
+         enabled: environment.production
+      }),
+   ],
+   providers: [
+      {
+         provide: ErrorHandler,
+         useClass: ErrorsHandler
+      }
+   ],
+   entryComponents: [],
+   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
