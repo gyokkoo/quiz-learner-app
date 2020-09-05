ea698df6a61d7a53fc814a68623912ed4be546e4
Gyokan <gyokan.nezhdetov@gmail.com>
2020-09-05 21:13:47 +0300

    Migrate template view to material

--- src/app/quizzes/edit/basic-info/edit-quiz-info.component.ts
+++ src/app/quizzes/edit/basic-info/edit-quiz-info.component.ts
@@ -7,22 +7,41 @@ import {
 import { QuizzesService, QuizDetails } from '../../quizzes.service';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { filter } from 'rxjs/operators';
+
+const minQuizTitleLength = 5;
+const maxQuizTitleLength = 100;
+const maxQuizDescriptionLength = 500;
 
 @Component({
   selector: 'app-edit-quiz-info',
   templateUrl: 'edit-quiz-info.component.html',
+  styleUrls: ['../../quiz-form.style.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class EditQuizInfoComponent implements OnInit {
   quiz: any;
 
-  quizInfoForm: FormGroup;
+  formGroup: FormGroup;
 
   loading: boolean;
 
   private id: string;
+
+  get isTitleValid(): boolean {
+    return (
+      !this.formGroup.controls['title'].valid &&
+      this.formGroup.controls['title'].touched
+    );
+  }
+
+  get isDescriptionValid(): boolean {
+    return (
+      !this.formGroup.controls['description'].valid &&
+      this.formGroup.controls['description'].touched
+    );
+  }
 
   constructor(
     private cdr: ChangeDetectorRef,
@@ -37,9 +56,20 @@ export class EditQuizInfoComponent implements OnInit {
     this.loading = true;
     this.quizzesService.getQuizById(this.id).subscribe((quizInfo) => {
       this.quiz = quizInfo;
-      this.quizInfoForm = this.formBuilder.group({
-        title: [this.quiz.name],
-        description: [this.quiz.description],
+      this.formGroup = this.formBuilder.group({
+        title: [
+          this.quiz.name,
+          [
+            Validators.required,
+            Validators.minLength(minQuizTitleLength),
+            Validators.maxLength(maxQuizTitleLength),
+          ],
+        ],
+
+        description: [
+          this.quiz.description,
+          [Validators.required, Validators.maxLength(maxQuizDescriptionLength)],
+        ],
       });
 
       this.loading = false;
@@ -48,11 +78,11 @@ export class EditQuizInfoComponent implements OnInit {
   }
 
   onSave(): void {
-    if (this.quizInfoForm.valid) {
+    if (this.formGroup.valid) {
       const quizDetails: QuizDetails = {
         id: this.id,
-        title: this.quizInfoForm.get('title').value.trim(),
-        description: this.quizInfoForm.get('description').value.trim(),
+        title: this.formGroup.get('title').value.trim(),
+        description: this.formGroup.get('description').value.trim(),
       };
 
       this.quizzesService
